4258d898930a05dab3a157b54eb5b4eeab81d4e6
Oleh Tsipotan <tsipotan.oleh@gmail.com>
2024-01-17 14:56:33 +0200

    refactor KafkaPaymentOrderProducerService

--- src/main/java/com/service/payment/service/KafkaPaymentOrderProducerService.java
+++ src/main/java/com/service/payment/service/KafkaPaymentOrderProducerService.java
@@ -1,28 +1,37 @@
 package com.service.payment.service;
 
+import com.domain.avro.model.AvroOrder;
+import com.service.payment.converter.OrderToAvroOrderConverter;
+import com.service.payment.exception.ServiceException;
 import com.service.payment.model.Order;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaPaymentOrderProducerService {
 
-    private final KafkaTemplate<UUID, Order> template;
+    private final KafkaTemplate<String, AvroOrder> template;
+
+    private final OrderToAvroOrderConverter converter;
 
     @Value("${kafka.payment-orders.topic}")
     private String topic;
 
     public void sendOrder(@NonNull Order order) {
-        log.info("Sending order: {}", order);
-        this.template.send(topic, order.getId(), order);
+        AvroOrder avroOrder = converter.convert(order);
+        try {
+            this.template.send(topic, String.valueOf(avroOrder.getId()), avroOrder);
+            log.info("Order sent to Kafka: {}", avroOrder);
+        } catch (KafkaException e) {
+            throw new ServiceException("Error sending order to Kafka", e);
+        }
     }
 
 
